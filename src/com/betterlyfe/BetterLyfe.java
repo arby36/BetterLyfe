e2f604009fdd87193a0ae154719b4e26fa5e727c
arby36 <arbybruce@gmail.com>
2020-04-09 22:39:39 -0400

    1.0.1

--- src/com/betterlyfe/BetterLyfe.java
+++ src/com/betterlyfe/BetterLyfe.java
@@ -1,7 +1,24 @@
 package com.betterlyfe;
 
+
 public class BetterLyfe {
+    public class User {
+        private String name;
+        public String setName(String arg) {
+            name = arg;
+            return name;
+        }
+        public String getName() {
+            return name;
+        }
+    }
+    public class Algorithm {
+
+    }
     public static void main(String[] args) {
-        System.out.print("Hello World!");
+        BetterLyfe instance = new BetterLyfe();
+        BetterLyfe.User user = instance.new User();
+        user.setName("Hubert");
+        System.out.print(user.getName());
     }
 }
